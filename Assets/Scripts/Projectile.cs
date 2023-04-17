04db85fbaa17cc8481169129c26408d417b8f26e
jacobchoi77 <jacobchoi77@gmail.com>
2023-04-17 17:36:14 +0900

    fsdf

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,25 +4,24 @@ public class Projectile : MonoBehaviour{
     public Vector2 moveSpeed = new Vector2(3f, 0);
 
     public int damage = 10;
-    private Rigidbody2D rb;
+    private Rigidbody2D _rb;
     public Vector2 knockback = new(0, 0);
 
     private void Awake(){
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    void Start(){
-        rb.velocity = new Vector2(moveSpeed.x * transform.localScale.x, moveSpeed.y);
+    private void Start(){
+        _rb.velocity = new Vector2(moveSpeed.x * transform.localScale.x, moveSpeed.y);
     }
 
     private void OnTriggerEnter2D(Collider2D other){
         var damageable = other.GetComponent<Damageable>();
-        if (damageable != null){
-            var deliveredKnockback =
-                transform.localScale.x > 0 ? knockback : new Vector2(-knockback.x, knockback.y);
-            var gotHit = damageable.Hit(damage, deliveredKnockback);
-            if (gotHit)
-                Destroy(gameObject);
-        }
+        if (damageable == null) return;
+        var deliveredKnockback =
+            transform.localScale.x > 0 ? knockback : new Vector2(-knockback.x, knockback.y);
+        var gotHit = damageable.Hit(damage, deliveredKnockback);
+        if (gotHit)
+            Destroy(gameObject);
     }
 }
